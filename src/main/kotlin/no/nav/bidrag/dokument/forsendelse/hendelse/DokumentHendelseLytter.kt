d812eaaf59f80b7432c2fed9aa3e00f017a88445
Ugur Cenar <cenar93@gmail.com>
2023-01-20 09:26:39 +0100

    Oppdater tester

--- src/main/kotlin/no/nav/bidrag/dokument/forsendelse/hendelse/DokumentHendelseLytter.kt
+++ src/main/kotlin/no/nav/bidrag/dokument/forsendelse/hendelse/DokumentHendelseLytter.kt
@@ -17,6 +17,7 @@ import no.nav.bidrag.dokument.forsendelse.utvidelser.erAlleFerdigstilt
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
 
 private val log = KotlinLogging.logger {}
 
@@ -24,6 +25,7 @@ private val log = KotlinLogging.logger {}
 class DokumentHendelseLytter(val objectMapper: ObjectMapper, val dokumentTjeneste: DokumentTjeneste, val oppdaterForsendelseTjeneste: OppdaterForsendelseTjeneste) {
 
     @KafkaListener(groupId = "bidrag-dokument-forsendelse", topics = ["\${TOPIC_DOKUMENT}"])
+    @Transactional
     fun prossesserDokumentHendelse(melding: ConsumerRecord<String, String>){
         val hendelse = tilDokumentHendelseObjekt(melding)
 
