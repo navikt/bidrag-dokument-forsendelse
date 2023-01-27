7d893c527b5378b87d1466d2611e53fdf0dc8703
Ugur Cenar <cenar93@gmail.com>
2023-01-27 12:52:02 +0100

    Oppdater datamodell og refaktorering

--- src/main/kotlin/no/nav/bidrag/dokument/forsendelse/utvidelser/DokumentUtvidelser.kt
+++ src/main/kotlin/no/nav/bidrag/dokument/forsendelse/utvidelser/DokumentUtvidelser.kt
@@ -6,7 +6,7 @@ import no.nav.bidrag.dokument.forsendelse.database.model.DokumentTilknyttetSom
 
 val Dokument.forsendelseIdMedPrefix get() = this.forsendelse.forsendelseIdMedPrefix
 fun List<Dokument>.hentDokument(dokumentreferanse: String?) = dokumenterIkkeSlettet.find { it.dokumentreferanse == dokumentreferanse }
-val List<Dokument>.erAlleFerdigstilt get() = dokumenterIkkeSlettet.all { it.dokumentStatus == DokumentStatus.FERDIGSTILT }
+val List<Dokument>.erAlleFerdigstilt get() = dokumenterIkkeSlettet.isNotEmpty() && dokumenterIkkeSlettet.all { it.dokumentStatus == DokumentStatus.FERDIGSTILT }
 val List<Dokument>.ikkeSlettetSortertEtterRekkefølge get() = dokumenterIkkeSlettet.sortedBy { it.rekkefølgeIndeks }
 val List<Dokument>.hoveddokument get() = dokumenterIkkeSlettet.find { it.tilknyttetSom == DokumentTilknyttetSom.HOVEDDOKUMENT }
 val List<Dokument>.vedlegger get() = dokumenterIkkeSlettet.filter { it.tilknyttetSom == DokumentTilknyttetSom.VEDLEGG }.sortedBy { it.rekkefølgeIndeks }
