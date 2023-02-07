b9d24ccd483f5772329d76fda419595699e06cef
Ugur Cenar <cenar93@gmail.com>
2023-02-07 16:32:54 +0100

    Støtte for å arkivere med samhandler

--- src/main/kotlin/no/nav/bidrag/dokument/forsendelse/hendelse/DokumentBestillingLytter.kt
+++ src/main/kotlin/no/nav/bidrag/dokument/forsendelse/hendelse/DokumentBestillingLytter.kt
@@ -1,8 +1,6 @@
 package no.nav.bidrag.dokument.forsendelse.hendelse
 
 import mu.KotlinLogging
-import net.javacrumbs.shedlock.spring.annotation.SchedulerLock
-import no.nav.bidrag.commons.security.SikkerhetsKontekst.medApplikasjonKontekst
 import no.nav.bidrag.dokument.dto.DokumentArkivSystemDto
 import no.nav.bidrag.dokument.forsendelse.consumer.BidragDokumentBestillingConsumer
 import no.nav.bidrag.dokument.forsendelse.consumer.dto.DokumentBestillingForespørsel
@@ -19,21 +17,19 @@ import no.nav.bidrag.dokument.forsendelse.model.Saksbehandler
 import no.nav.bidrag.dokument.forsendelse.service.SaksbehandlerInfoManager
 import no.nav.bidrag.dokument.forsendelse.service.dao.DokumentTjeneste
 import no.nav.bidrag.dokument.forsendelse.utvidelser.hentDokument
-import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 import org.springframework.transaction.event.TransactionPhase
 import org.springframework.transaction.event.TransactionalEventListener
-import java.util.concurrent.TimeUnit
 import javax.transaction.Transactional
 
 private val LOGGER = KotlinLogging.logger {}
 
 @Component
 class DokumentBestillingLytter(
-        val dokumentBestillingKonsumer: BidragDokumentBestillingConsumer,
-        val forsendelseRepository: ForsendelseRepository,
-        val dokumentTjeneste: DokumentTjeneste,
-        val saksbehandlerInfoManager: SaksbehandlerInfoManager
+    val dokumentBestillingKonsumer: BidragDokumentBestillingConsumer,
+    val forsendelseRepository: ForsendelseRepository,
+    val dokumentTjeneste: DokumentTjeneste,
+    val saksbehandlerInfoManager: SaksbehandlerInfoManager
 ) {
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
@@ -41,9 +37,9 @@ class DokumentBestillingLytter(
     fun bestill(dokumentBestilling: DokumentBestilling) {
         val (forsendelseId, dokumentreferanse) = dokumentBestilling
         val forsendelse = forsendelseRepository.medForsendelseId(forsendelseId)
-                ?: throw KunneIkkBestilleDokument("Fant ikke forsendelse $forsendelseId")
+            ?: throw KunneIkkBestilleDokument("Fant ikke forsendelse $forsendelseId")
         val dokument = forsendelse.dokumenter.hentDokument(dokumentreferanse)
-                ?: throw KunneIkkBestilleDokument("Fant ikke dokument med dokumentreferanse $dokumentreferanse i forsendelse ${forsendelse.forsendelseId}")
+            ?: throw KunneIkkBestilleDokument("Fant ikke dokument med dokumentreferanse $dokumentreferanse i forsendelse ${forsendelse.forsendelseId}")
         if (dokument.dokumentmalId.isNullOrEmpty()) throw KunneIkkBestilleDokument("Dokument med dokumentreferanse $dokumentreferanse mangler dokumentmalId")
 
         val bestilling = tilForespørsel(forsendelse, dokument)
@@ -54,60 +50,46 @@ class DokumentBestillingLytter(
             LOGGER.info { "Bestilte ny dokument med mal ${dokument.dokumentmalId} og tittel ${bestilling.tittel} for dokumentreferanse ${bestilling.dokumentreferanse}. Dokumentet er arkivert i ${respons?.arkivSystem?.name}" }
 
             dokumentTjeneste.lagreDokument(
-                    dokument.copy(
-                            arkivsystem = when (respons?.arkivSystem) {
-                                DokumentArkivSystemDto.MIDLERTIDLIG_BREVLAGER -> DokumentArkivSystem.MIDLERTIDLIG_BREVLAGER
-                                else -> DokumentArkivSystem.UKJENT
-                            },
-                            dokumentStatus = DokumentStatus.UNDER_PRODUKSJON
-                    )
+                dokument.copy(
+                    arkivsystem = when (respons?.arkivSystem) {
+                        DokumentArkivSystemDto.MIDLERTIDLIG_BREVLAGER -> DokumentArkivSystem.MIDLERTIDLIG_BREVLAGER
+                        else -> DokumentArkivSystem.UKJENT
+                    },
+                    dokumentStatus = DokumentStatus.UNDER_PRODUKSJON
+                )
             )
         } catch (e: Exception) {
             dokumentTjeneste.lagreDokument(
-                    dokument.copy(dokumentStatus = DokumentStatus.BESTILLING_FEILET)
+                dokument.copy(dokumentStatus = DokumentStatus.BESTILLING_FEILET)
             )
             LOGGER.error(e) { "Det skjedde en feil ved bestilling av dokumentmal ${dokument.dokumentmalId} for dokumentreferanse $dokumentreferanse og forsendelseId $forsendelseId" }
         }
 
     }
 
-    @Scheduled(fixedDelay = 30, timeUnit = TimeUnit.MINUTES, initialDelay = 10)
-    @SchedulerLock(name = "bestillFeiledeDokumenterPaNytt", lockAtLeastFor = "10m")
-    fun bestillFeiledeDokumenterPåNytt() {
-
-        val dokumenter = dokumentTjeneste.hentDokumenterSomHarStatusBestillingFeilet()
-        LOGGER.info { "Fant ${dokumenter.size} dokumenter som har status ${DokumentStatus.BESTILLING_FEILET.name}. Prøver å bestille dokumentene på nytt." }
-
-        medApplikasjonKontekst {
-            dokumenter.forEach {
-                bestill(DokumentBestilling(it.forsendelse.forsendelseId!!, it.dokumentreferanse))
-            }
-        }
-    }
-
     private fun tilForespørsel(forsendelse: Forsendelse, dokument: Dokument): DokumentBestillingForespørsel {
         val saksbehandlerIdent = if (saksbehandlerInfoManager.erApplikasjonBruker()) forsendelse.opprettetAvIdent else null
         return DokumentBestillingForespørsel(
-                dokumentreferanse = dokument.dokumentreferanse,
-                saksnummer = forsendelse.saksnummer,
-                tittel = dokument.tittel,
-                gjelderId = forsendelse.gjelderIdent,
-                enhet = forsendelse.enhet,
-                språk = dokument.språk ?: forsendelse.språk,
-                saksbehandler = saksbehandlerIdent?.let { Saksbehandler(it) },
-                mottaker = forsendelse.mottaker?.let { mottaker ->
-                    MottakerTo(mottaker.ident, mottaker.navn, mottaker.språk, adresse = mottaker.adresse?.let {
-                        MottakerAdresseTo(
-                                adresselinje1 = it.adresselinje1,
-                                adresselinje2 = it.adresselinje2,
-                                adresselinje3 = it.adresselinje3,
-                                bruksenhetsnummer = it.bruksenhetsnummer,
-                                postnummer = it.postnummer,
-                                landkode = it.landkode,
-                                landkode3 = it.landkode3,
-                                poststed = it.poststed
-                        )
-                    })
+            dokumentreferanse = dokument.dokumentreferanse,
+            saksnummer = forsendelse.saksnummer,
+            tittel = dokument.tittel,
+            gjelderId = forsendelse.gjelderIdent,
+            enhet = forsendelse.enhet,
+            språk = dokument.språk ?: forsendelse.språk,
+            saksbehandler = saksbehandlerIdent?.let { Saksbehandler(it) },
+            mottaker = forsendelse.mottaker?.let { mottaker ->
+                MottakerTo(mottaker.ident, mottaker.navn, mottaker.språk, adresse = mottaker.adresse?.let {
+                    MottakerAdresseTo(
+                        adresselinje1 = it.adresselinje1,
+                        adresselinje2 = it.adresselinje2,
+                        adresselinje3 = it.adresselinje3,
+                        bruksenhetsnummer = it.bruksenhetsnummer,
+                        postnummer = it.postnummer,
+                        landkode = it.landkode,
+                        landkode3 = it.landkode3,
+                        poststed = it.poststed
+                    )
                 })
+            })
     }
 }
---
+++ src/main/kotlin/no/nav/bidrag/dokument/forsendelse/hendelse/DokumentSkedulering.kt
@@ -0,0 +1,32 @@
+package no.nav.bidrag.dokument.forsendelse.hendelse
+
+import mu.KotlinLogging
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock
+import no.nav.bidrag.commons.security.SikkerhetsKontekst
+import no.nav.bidrag.dokument.forsendelse.database.model.DokumentStatus
+import no.nav.bidrag.dokument.forsendelse.model.DokumentBestilling
+import no.nav.bidrag.dokument.forsendelse.service.dao.DokumentTjeneste
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+import java.util.concurrent.TimeUnit
+
+private val LOGGER = KotlinLogging.logger {}
+
+@Component
+class DokumentSkedulering(private val dokumentTjeneste: DokumentTjeneste, private val bestillingLytter: DokumentBestillingLytter) {
+
+
+    @Scheduled(fixedDelay = 30, timeUnit = TimeUnit.MINUTES, initialDelay = 10)
+    @SchedulerLock(name = "bestillFeiledeDokumenterPaNytt", lockAtLeastFor = "10m")
+    fun bestillFeiledeDokumenterPåNytt() {
+
+        val dokumenter = dokumentTjeneste.hentDokumenterSomHarStatusBestillingFeilet()
+        LOGGER.info { "Fant ${dokumenter.size} dokumenter som har status ${DokumentStatus.BESTILLING_FEILET.name}. Prøver å bestille dokumentene på nytt." }
+
+        SikkerhetsKontekst.medApplikasjonKontekst {
+            dokumenter.forEach {
+                bestillingLytter.bestill(DokumentBestilling(it.forsendelse.forsendelseId!!, it.dokumentreferanse))
+            }
+        }
+    }
+}
