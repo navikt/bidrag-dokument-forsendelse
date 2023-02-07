f3dc752264e323a58ac930bca210602fc0d0c0ac
Ugur Cenar <cenar93@gmail.com>
2023-02-07 16:48:25 +0100

    Støtte for å arkivere med samhandler

--- src/main/kotlin/no/nav/bidrag/dokument/forsendelse/aop/HendelseCorrelationAspect.kt
+++ src/main/kotlin/no/nav/bidrag/dokument/forsendelse/aop/HendelseCorrelationAspect.kt
@@ -32,16 +32,29 @@ class HendelseCorrelationAspect(private val objectMapper: ObjectMapper) {
 
     }
 
+    @Before(value = "execution(* no.nav.bidrag.dokument.forsendelse.hendelse.DokumentSkedulering.bestillFeiledeDokumenterPåNytt(..))")
+    fun leggKorreleringsIdPåSkjedulering(joinPoint: JoinPoint) {
+        val tilfeldigVerdi = UUID.randomUUID().toString().subSequence(0, 8)
+        val korrelasjonsId = "${tilfeldigVerdi}_bestillFeiledeDokumenterPaNytt"
+        MDC.put(CORRELATION_ID_HEADER, CorrelationId.existing(korrelasjonsId).get())
+    }
+
     private fun hentSporingFraHendelse(hendelse: ConsumerRecord<String, String>): String? {
         return try {
             val jsonNode = objectMapper.readTree(hendelse.value())
             jsonNode["sporingId"].asText()
-        } catch (e: Exception){
+        } catch (e: Exception) {
             LOGGER.error("Det skjedde en feil ved konverting av melding fra hendelse", e)
             null
         }
     }
+
     @After(value = "execution(* no.nav.bidrag.dokument.forsendelse.hendelse.DokumentHendelseLytter.*(..))")
+    fun clearCorrelationIdFromKafkaListener(joinPoint: JoinPoint) {
+        MDC.clear()
+    }
+
+    @After(value = "execution(* no.nav.bidrag.dokument.forsendelse.hendelse.DokumentSkedulering.*(..))")
     fun clearCorrelationIdFromScheduler(joinPoint: JoinPoint) {
         MDC.clear()
     }
