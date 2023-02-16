e14a8f71d4fe6d7cd4765c6d0f537ec762eacf45
Ugur Cenar <cenar93@gmail.com>
2023-02-16 13:09:40 +0100

    Forsendelse med tema

--- src/main/resources/db/migration/V1.1.0__forsendelse_kolonne_tema_batchid.sql
+++ src/main/resources/db/migration/V1.1.0__forsendelse_kolonne_tema_batchid.sql
@@ -1,4 +1,6 @@
 alter table forsendelse
+    drop column if exists tema;
+alter table forsendelse
     add column if not exists tema text not null default 'BID';
 alter table forsendelse
     add column if not exists batch_id text;
