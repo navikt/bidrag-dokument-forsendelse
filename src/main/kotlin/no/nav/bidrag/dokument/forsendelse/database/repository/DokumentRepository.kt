a6d43fcecd728bd8a890e845beeae2c856fb6ca9
Ugur Cenar <cenar93@gmail.com>
2023-03-16 11:57:31 +0100

    Hent bare dokumenter som bestilling feilet for forsendelse med status under prod

--- src/main/kotlin/no/nav/bidrag/dokument/forsendelse/database/repository/DokumentRepository.kt
+++ src/main/kotlin/no/nav/bidrag/dokument/forsendelse/database/repository/DokumentRepository.kt
@@ -10,6 +10,6 @@ interface DokumentRepository : CrudRepository<Dokument, Long> {
     @Query("select d from dokument d where d.dokumentreferanseOriginal = :dokumentreferanse or d.dokumentId = :dokumentId")
     fun hentDokumenterMedDokumentreferanse(dokumentreferanse: String, dokumentId: Long?): List<Dokument>
 
-    @Query("select d from dokument d where d.dokumentStatus = 'BESTILLING_FEILET'")
+    @Query("select d from dokument d where d.dokumentStatus = 'BESTILLING_FEILET' and d.forsendelse.status = 'UNDER_PRODUKSJON'")
     fun hentDokumenterSomHarStatusBestillingFeilet(): List<Dokument>
 }
--- src/test/kotlin/no/nav/bidrag/dokument/forsendelse/hendelse/BestillFeiledeDokumentereSkeduleringTest.kt
+++ src/test/kotlin/no/nav/bidrag/dokument/forsendelse/hendelse/BestillFeiledeDokumentereSkeduleringTest.kt
@@ -3,7 +3,10 @@ package no.nav.bidrag.dokument.forsendelse.hendelse
 import no.nav.bidrag.dokument.forsendelse.TestContainerRunner
 import no.nav.bidrag.dokument.forsendelse.database.model.DokumentArkivSystem
 import no.nav.bidrag.dokument.forsendelse.database.model.DokumentStatus
+import no.nav.bidrag.dokument.forsendelse.database.model.ForsendelseStatus
 import no.nav.bidrag.dokument.forsendelse.utils.nyttDokument
+import no.nav.bidrag.dokument.forsendelse.utils.opprettForsendelse2
+import no.nav.bidrag.dokument.forsendelse.utvidelser.hoveddokument
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -23,43 +26,102 @@ class BestillFeiledeDokumentereSkeduleringTest : TestContainerRunner() {
 
     @Test
     fun `Skal oppdatere status på dokument til UNDER_REDIGERING ved mottatt hendelse`() {
-        testDataManager.opprettOgLagreForsendelse {
-            +nyttDokument(
-                dokumentreferanseOriginal = null,
-                journalpostId = null,
-                dokumentStatus = DokumentStatus.BESTILLING_FEILET,
-                tittel = "FORSENDELSE 1",
-                arkivsystem = DokumentArkivSystem.UKJENT,
-                dokumentMalId = "MAL1"
+        val forsendelse1 = testDataManager.lagreForsendelse(
+            opprettForsendelse2(
+                dokumenter = listOf(
+                    nyttDokument(
+                        dokumentreferanseOriginal = null,
+                        journalpostId = null,
+                        dokumentStatus = DokumentStatus.BESTILLING_FEILET,
+                        tittel = "FORSENDELSE 1",
+                        arkivsystem = DokumentArkivSystem.UKJENT,
+                        dokumentMalId = "MAL1"
+                    )
+                )
             )
-        }
-        testDataManager.opprettOgLagreForsendelse {
-            +nyttDokument(
-                dokumentreferanseOriginal = null,
-                journalpostId = null,
-                dokumentStatus = DokumentStatus.BESTILLING_FEILET,
-                tittel = "FORSENDELSE 1",
-                arkivsystem = DokumentArkivSystem.UKJENT,
-                dokumentMalId = "MAL2"
+        )
+        val forsendelse2 = testDataManager.lagreForsendelse(
+            opprettForsendelse2(
+                dokumenter = listOf(
+                    nyttDokument(
+                        dokumentreferanseOriginal = null,
+                        journalpostId = null,
+                        dokumentStatus = DokumentStatus.BESTILLING_FEILET,
+                        tittel = "FORSENDELSE 1",
+                        arkivsystem = DokumentArkivSystem.UKJENT,
+                        dokumentMalId = "MAL2"
+                    )
+                )
             )
-        }
-
-        testDataManager.opprettOgLagreForsendelse {
-            +nyttDokument(
-                dokumentreferanseOriginal = null,
-                journalpostId = null,
-                dokumentStatus = DokumentStatus.UNDER_REDIGERING,
-                tittel = "FORSENDELSE 1",
-                arkivsystem = DokumentArkivSystem.UKJENT,
-                dokumentMalId = "MAL2"
+        )
+        testDataManager.lagreForsendelse(
+            opprettForsendelse2(
+                dokumenter = listOf(
+                    nyttDokument(
+                        dokumentreferanseOriginal = null,
+                        journalpostId = null,
+                        dokumentStatus = DokumentStatus.UNDER_REDIGERING,
+                        tittel = "FORSENDELSE 1",
+                        arkivsystem = DokumentArkivSystem.UKJENT,
+                        dokumentMalId = "MAL3"
+                    )
+                )
             )
-        }
+        )
 
         skedulering.bestillFeiledeDokumenterPåNytt()
 
-        stubUtils.Valider().bestillDokumentKaltMed("MAL1", "\"saksbehandler\":{\"ident\":\"Z999444\",\"navn\":null}")
-        stubUtils.Valider().bestillDokumentKaltMed("MAL2", "\"saksbehandler\":{\"ident\":\"Z999444\",\"navn\":null}")
+        stubUtils.Valider().bestillDokumentKaltMed(
+            "MAL1",
+            "\"saksbehandler\":{\"ident\":\"Z999444\",\"navn\":null}",
+            "\"dokumentreferanse\":\"${forsendelse1.dokumenter.hoveddokument!!.dokumentreferanse}\""
+        )
+        stubUtils.Valider().bestillDokumentKaltMed(
+            "MAL2",
+            "\"saksbehandler\":{\"ident\":\"Z999444\",\"navn\":null}",
+            "\"dokumentreferanse\":\"${forsendelse2.dokumenter.hoveddokument!!.dokumentreferanse}\""
+        )
         stubUtils.Valider().bestillDokumentIkkeKalt("MAL3")
+
+    }
+
+    @Test
+    fun `Skal ignorere dokumenter hvor forsendelse ikke har status UNDER_PRODUKSJON`() {
+        testDataManager.lagreForsendelse(
+            opprettForsendelse2(
+                status = ForsendelseStatus.SLETTET,
+                dokumenter = listOf(
+                    nyttDokument(
+                        dokumentreferanseOriginal = null,
+                        journalpostId = null,
+                        dokumentStatus = DokumentStatus.BESTILLING_FEILET,
+                        tittel = "FORSENDELSE 1",
+                        arkivsystem = DokumentArkivSystem.UKJENT,
+                        dokumentMalId = "MAL1"
+                    )
+                )
+            )
+        )
+        testDataManager.lagreForsendelse(
+            opprettForsendelse2(
+                status = ForsendelseStatus.SLETTET,
+                dokumenter = listOf(
+                    nyttDokument(
+                        dokumentreferanseOriginal = null,
+                        journalpostId = null,
+                        dokumentStatus = DokumentStatus.BESTILLING_FEILET,
+                        tittel = "FORSENDELSE 1",
+                        arkivsystem = DokumentArkivSystem.UKJENT,
+                        dokumentMalId = "MAL2"
+                    )
+                )
+            )
+        )
+
+        skedulering.bestillFeiledeDokumenterPåNytt()
+
+        stubUtils.Valider().bestillDokumentIkkeKalt("MAL1")
+        stubUtils.Valider().bestillDokumentIkkeKalt("MAL2")
 
     }
 }
