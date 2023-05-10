40b57544c0674322b34bd0455b0319978a32170f
Ugur Cenar <cenar93@gmail.com>
2023-05-10 07:11:15 +0200

    Oppdater forsendelse status ved ingen distribusjon

--- src/main/kotlin/no/nav/bidrag/dokument/forsendelse/database/repository/ForsendelseRepository.kt
+++ src/main/kotlin/no/nav/bidrag/dokument/forsendelse/database/repository/ForsendelseRepository.kt
@@ -18,6 +18,6 @@ interface ForsendelseRepository : CrudRepository<Forsendelse, Long> {
     @Query("select f from forsendelse f where f.status = 'DISTRIBUERT' and f.distribusjonKanal is null and f.distribuertTidspunkt <= :olderThan order by f.distribuertTidspunkt asc")
     fun hentDistribuerteForsendelseUtenKanal(pageable: Pageable, olderThan: LocalDateTime): List<Forsendelse>
 
-    @Query("select f from forsendelse f where f.status = 'FERDIGSTILT' and f.journalpostIdFagarkiv is not null and f.forsendelseType = 'UTGÅENDE' and f.distribusjonKanal is null ")
+    @Query("select f from forsendelse f where f.status = 'FERDIGSTILT' and f.journalpostIdFagarkiv is not null and f.forsendelseType = 'UTGÅENDE' and f.distribusjonKanal is null")
     fun hentFerdigstilteIkkeDistribuert(): List<Forsendelse>
 }
