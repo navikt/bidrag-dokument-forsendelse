5d9c559b85ba1453e2fb26c63b685b0b95e4a1a2
Ugur Cenar <cenar93@gmail.com>
2023-11-29 14:06:22 +0100

    Kortnavn til visningsnavn og endring av søkt av type

--- src/main/kotlin/no/nav/bidrag/dokument/forsendelse/mapper/ForespørselMapper.kt
+++ src/main/kotlin/no/nav/bidrag/dokument/forsendelse/mapper/ForespørselMapper.kt
@@ -34,7 +34,7 @@ object ForespørselMapper {
     }
 
     fun MottakerTo.tilMottakerDo(person: PersonDto?, språk: String) = Mottaker(
-        navn = this.navn ?: person?.visningsnavn,
+        navn = this.navn ?: person?.kortnavn,
         ident = this.ident,
         språk = språk.uppercase(),
         identType = when (this.identType) {
