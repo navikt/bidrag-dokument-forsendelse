a5ce7cd0f804bb0683714ea9e3f42d21064804d9
Ugur Cenar <cenar93@gmail.com>
2023-12-01 11:40:51 +0100

    Bruk commons saksbehandlernavn provider for å hente navn

--- src/main/kotlin/no/nav/bidrag/dokument/forsendelse/service/SaksbehandlerInfoManager.kt
+++ src/main/kotlin/no/nav/bidrag/dokument/forsendelse/service/SaksbehandlerInfoManager.kt
@@ -2,21 +2,21 @@ package no.nav.bidrag.dokument.forsendelse.service
 
 import no.nav.bidrag.commons.security.SikkerhetsKontekst.erIApplikasjonKontekst
 import no.nav.bidrag.commons.security.utils.TokenUtils
-import no.nav.bidrag.dokument.forsendelse.consumer.BidragOrganisasjonConsumer
+import no.nav.bidrag.commons.service.organisasjon.SaksbehandlernavnProvider
 import no.nav.bidrag.dokument.forsendelse.model.Saksbehandler
 import org.springframework.stereotype.Service
 
 val FORSENDELSE_APP_ID = "bidrag-dokument-forsendelse"
 
 @Service
-class SaksbehandlerInfoManager(private val bidragOrganisasjonConsumer: BidragOrganisasjonConsumer) {
+class SaksbehandlerInfoManager {
     fun hentSaksbehandlerBrukerId(): String? = if (erIApplikasjonKontekst()) FORSENDELSE_APP_ID else TokenUtils.hentBruker()
 
     fun hentSaksbehandler(): Saksbehandler? {
         val saksbehandlerIdent = hentSaksbehandlerBrukerId() ?: return null
         return try {
-            val saksbehandlerNavn = bidragOrganisasjonConsumer.hentSaksbehandlerInfo(saksbehandlerIdent)?.navn
-            Saksbehandler(saksbehandlerIdent, saksbehandlerNavn)
+            val saksbehandlerNavn = SaksbehandlernavnProvider.hentSaksbehandlernavn(saksbehandlerIdent)
+            Saksbehandler(saksbehandlerIdent, saksbehandlerNavn ?: saksbehandlerIdent)
         } catch (e: Exception) {
             Saksbehandler(saksbehandlerIdent, saksbehandlerIdent)
         }
