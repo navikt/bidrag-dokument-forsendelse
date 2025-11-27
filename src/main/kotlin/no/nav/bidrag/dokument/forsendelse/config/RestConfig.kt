a3e1987dfa22bbf5d448ae9d8836c08df35d22e6
Ugur Alpay Cenar <cenar93@gmail.com>
2025-11-27 08:38:53 +0100

    Oppgrader felles

--- src/main/kotlin/no/nav/bidrag/dokument/forsendelse/config/RestConfig.kt
+++ src/main/kotlin/no/nav/bidrag/dokument/forsendelse/config/RestConfig.kt
@@ -7,6 +7,7 @@ import no.nav.bidrag.commons.security.api.EnableSecurityConfiguration
 import no.nav.bidrag.commons.service.KodeverkProvider
 import no.nav.bidrag.commons.web.config.RestOperationsAzure
 import no.nav.bidrag.commons.web.interceptor.BearerTokenClientInterceptor
+import no.nav.bidrag.transport.felles.commonObjectmapper
 import org.apache.hc.client5.http.impl.classic.HttpClientBuilder
 import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder
 import org.apache.hc.core5.http.io.SocketConfig
@@ -21,6 +22,9 @@ import org.springframework.context.annotation.Scope
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory
 import org.springframework.http.client.observation.DefaultClientRequestObservationConvention
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
+import org.springframework.http.converter.xml.MappingJackson2XmlHttpMessageConverter
+import org.springframework.web.client.RestTemplate
 
 @Configuration
 @EnableSecurityConfiguration
@@ -47,14 +51,35 @@ class RestConfig(
     fun restOperationsJwtBearerNoBuffer(
         restTemplateBuilder: RestTemplateBuilder,
         bearerTokenClientInterceptor: BearerTokenClientInterceptor,
-    ) = restTemplateBuilder
-        .requestFactory { _ ->
-            val sc = SocketConfig.custom().setSoTimeout(Timeout.ofMinutes(5)).build()
-            val pb = PoolingHttpClientConnectionManagerBuilder.create().setDefaultSocketConfig(sc).build()
-            val connectionManager = HttpClientBuilder.create().setConnectionManager(pb).build()
-            HttpComponentsClientHttpRequestFactory(connectionManager)
-        }.additionalInterceptors(bearerTokenClientInterceptor)
-        .build()
+    ): RestTemplate {
+        val restTemplate =
+            restTemplateBuilder
+                .requestFactoryBuilder {
+                    val sc = SocketConfig.custom().setSoTimeout(Timeout.ofMinutes(5)).build()
+                    val pb = PoolingHttpClientConnectionManagerBuilder.create().setDefaultSocketConfig(sc).build()
+                    val connectionManager = HttpClientBuilder.create().setConnectionManager(pb).build()
+                    HttpComponentsClientHttpRequestFactory(connectionManager)
+                }.additionalInterceptors(bearerTokenClientInterceptor)
+                .build()
+        configureJackson(restTemplate)
+        return restTemplate
+    }
+
+    private fun configureJackson(restTemplate: RestTemplate) {
+        restTemplate.messageConverters
+            .stream()
+            .filter { obj -> MappingJackson2HttpMessageConverter::class.java.isInstance(obj) }
+            .map { obj -> MappingJackson2HttpMessageConverter::class.java.cast(obj) }
+            .findFirst()
+            .ifPresent { converter: MappingJackson2HttpMessageConverter ->
+                converter.objectMapper = commonObjectmapper
+            }
+
+        restTemplate.messageConverters =
+            restTemplate.messageConverters
+                .filter { obj -> !MappingJackson2XmlHttpMessageConverter::class.java.isInstance(obj) }
+                .toMutableList()
+    }
 }
 
 @Profile("nais")
